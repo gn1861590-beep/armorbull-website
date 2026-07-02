cc4f4752bef7ab3eaf99c286f8bf60b2100b7c77
gn1861590-beep <gn1861590@gmail.com>
2026-07-02 21:39:22 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-const toggle=document.querySelector('.mobile-toggle');const menu=document.querySelector('.menu');if(toggle){toggle.addEventListener('click',()=>menu.classList.toggle('open'))}
+document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{e.preventDefault();alert('請加入 LINE：@bgq1409j，或來電 06-2438319');}));
